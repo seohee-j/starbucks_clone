7400c7204bdc29091568099d9bc3d28149432000
seohee-j <jeongseohee.dev@gmail.com>
2022-01-10 09:47:29 +0900

    헤더와 드롭다운 메뉴 - 전역 배지(GSAP) (2)

--- js/main.js
+++ js/main.js
@@ -18,6 +18,25 @@ searchInputEl.addEventListener('blur', function() {
 
 const badgeEl = document.querySelector('header .badges');
 
-window.addEventListener('scroll', function() {
-  console.log('scroll!');
-});
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if(window.scrollY > 500) {
+    // 배지 숨기기
+    // badgeEl.style.display = 'none'
+
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    // 배지 보이기
+    // badgeEl.style.display = 'block'
+
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+// _.throttle(함수, 시간)
